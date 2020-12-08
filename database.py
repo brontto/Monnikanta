5a464b56bdb99768642dce6e49e6e630a11fd251
brontto <bronttosaur@gmail.com>
2020-12-08 17:54:40 +0200

    korjauksia koodiin

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from werkzeug.security import generate_password_hash
 
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
